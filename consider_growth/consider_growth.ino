d9de08cff33c4a524b16ccae6c1af0586daadb01
Chris L <chris.luginbuhl@gmail.com>
2017-11-25 20:04:50 -0500

    Added more comments to code + title block.

--- consider_growth/consider_growth.ino
+++ consider_growth/consider_growth.ino
@@ -1,13 +1,27 @@
-/*
-   message key:
-   0: I am not sitting
-   1: I am sitting
-   2: You are not sitting
-   3: You are sitting
-   4: Ring the bell to begin
-   5: Ring the bell to end
+    /*******************************************************           
+    * Consider Growth -- Accountability and Community for  *
+    * nurturing new habits; meditation, music, business.   *
+    *                                                      *
+    *                                                      *
+    * DIGF-6037-001 Creation & Computation                 *                                       
+    *                                                      *  
+    * Assignment 4 Nov 24, 2017                            *
+    *                                                      * 
+    * Karo, Dave, Chris                                    *   
+    *                                                      *   
+    * Purpose:  Interfaces with website for start time,    *
+    * lights up your cushion when your accountability      *
+    * is seated. Triggers a web-based animation when you   *
+    * sit.                                                 *
+    *                                                      *   
+    * Usage:                                               *   
+    *  requires wifi credentials. Set MY_NAME & YOUR_NAME  *           
+    *  for each user                                       *
+    *                                                      *
+    * Credit: Makes use of Kate and Nick's example code for*  
+    * communicating with Pubnub                            *
+    /*******************************************************/  
 
-*/
 #include <ArduinoJson.h>
 #include <SPI.h>
 
@@ -16,44 +30,41 @@
 #include <PubNub.h>
 
 //OCADU Wifi credentials below
-static char ssid[] = "Chris's Wi-Fi Network 2.4G"; 
-static char pass[] = "summer99";
-//static char ssid[] = "ocadu-embedded";      //SSID of the wireless network
-//static char pass[] = "internetofthings";    //password of that network
+static char ssid[] = "ocadu-embedded";      //SSID of the wireless network
+static char pass[] = "internetofthings";    //password of that network
 int status = WL_IDLE_STATUS;                // the Wifi radio's status
 
 
 //Chris's pubnub account keys below
-const static char pubkey[] = "pub-c-31315745-29a7-4863-bc2a-97a528df3c93";  //get this from your PUbNub account
-const static char subkey[] = "sub-c-ac854de4-c885-11e7-9695-d62da049879f";  //get this from your PubNub account
+const static char pubkey[] = "pub-c-31315745-29a7-4863-bc2a-97a528df3c93";  //from PubNub account
+const static char subkey[] = "sub-c-ac854de4-c885-11e7-9695-d62da049879f";  //from PubNub account
 
-//COuld have user1 publishing to channel1, and listening to channel 2, with user2 publishing to channel 2, listening to channel 1
 const static char pubChannel[] = "channel1"; //choose a name for the channel to publish messages to
-const static char subChannel[] = "channel1"; //choose a name for the channel to publish messages to
+const static char subChannel[] = "channel1"; //choose a name for the channel to read messages from
 
-const int solenoid = 11;
-const int el = 10;
-const int cushionPin = 9;
-const int NUM_RINGS = 3;
-const String MY_NAME = "chrisMessage";
-const String YOUR_NAME = "karoMessage";
+const int solenoid = 11;   //this pin switches the solenoid that rings the bell.
+const int el = 10;         //this pin switches on/off the EL wire which illuminates the cushion
+const int cushionPin = 9;  //this pin reads from the sandwich switch in the cushion.
+const int NUM_RINGS = 3;   //number of times to ring the bell to start/end a session.
+const String MY_NAME = "chrisMessage"; //this is the Key associated with the messages I publish.
+const String YOUR_NAME = "karoMessage";//this is the key associated with messages my partner publishes.
 
-const int I_AM_NOT_SITTING = 0;
+const int I_AM_NOT_SITTING = 0;  //these are the values paired with the above keys that are published to pubnub 
 const int I_AM_SITTING = 1;
-const int YOU_ARE_NOT_SITTING = 2;
-const int YOU_ARE_SITTING = 3;
-const int BEGIN_SITTING = 4;
+const int YOU_ARE_NOT_SITTING = 0;
+const int YOU_ARE_SITTING = 1;
+const int BEGIN_SITTING = 4;     //these two values are published by the server
 const int END_SITTING = 5;
 
-int cushionVal = 1;  
-int cushionPrev = HIGH;
-int prevMsg = 0;
+int cushionVal = 1;      //Current state of cushion. 
+int cushionPrev = HIGH;  //initial state of switch is on. That way, if it's off, we'll send a signal to pubnub indicating the "change"
+int prevMsg = 0;         //the content of the message we'll send.
 
 unsigned long lastRefresh = 0;
 int publishRate = 1000;
 
-int myMsg;
-int yourMsg;
+int myMsg;                //myMsg is what I publish using MY_NAME
+int yourMsg;              //the message I receive from YOUR_NAME on pubnub.
 
 void setup()
 {
@@ -66,17 +77,19 @@ void setup()
 
 void loop()
 {
+//in this loop, we publish only when something has changed. We read from pubnub on a fixed interval.
+  
   cushionVal = digitalRead(cushionPin);
 
-  if ((cushionVal == LOW) && (cushionPrev == HIGH)) //if sitting, trigger the feed update with a button, uses both current and prev value to only change on the switch
+  if ((cushionVal == LOW) && (cushionPrev == HIGH)) //LOW = seated. If there is a change and I am now sitting, trigger the feed update. 
   {
-    myMsg = 1;
+    myMsg = I_AM_SITTING;
     publishToPubNub(myMsg);
-    digitalWrite(el, LOW);
+    digitalWrite(el, LOW); //turn off el wire to reduce distractions when I am sitting
     Serial.print("My message: ");
     Serial.println(myMsg);
-  } else if ((cushionVal == HIGH) && (cushionPrev == LOW))  {
-    myMsg = 0;
+  } else if ((cushionVal == HIGH) && (cushionPrev == LOW))  { //if I get up, send a message to pubnub so that my animation stops.
+    myMsg = I_AM_NOT_SITTING;
     publishToPubNub(myMsg);
     Serial.print("My message: ");
     Serial.println(myMsg);
@@ -87,29 +100,28 @@ void loop()
     readFromPubNub();
     if (yourMsg != prevMsg) {
       switch (yourMsg) {
-        case YOU_ARE_SITTING:
+        case YOU_ARE_SITTING:           //turn on the EL wire to remind me that our session has started and my partner is sitting.
           if (cushionVal == HIGH) {
             digitalWrite(el, HIGH);
           } else {
             digitalWrite(el, LOW);
           }
           break;
-        case YOU_ARE_NOT_SITTING:
+        case YOU_ARE_NOT_SITTING:      //turn off the EL wire if my partner is not sitting.
           digitalWrite(el, LOW);
           break;
-        case BEGIN_SITTING:  
+        case BEGIN_SITTING:           //ring the bell if the server starts the session
           ding();
           break;
-        case END_SITTING:
+        case END_SITTING:             //ring the bell if the server ends the session
           ding();
-          while(true); //do nothing forever
+          while(true); //then do nothing forever
       }
       
     }
 
     Serial.print("Your message: ");
-    Serial.println(yourMsg);
-    
+    Serial.println(yourMsg);    
     lastRefresh = millis();
     prevMsg = yourMsg;
   }
@@ -221,10 +233,10 @@ void readFromPubNub()
 void ding() {
   for (int i = 0; i < NUM_RINGS; i++) {
     digitalWrite(solenoid, HIGH);
-    delay(50);
+    delay(50);              //ring works best when the solenoid is extended and retracted quickly.
     Serial.println("ding!");
     digitalWrite(solenoid, LOW);
-    delay(2500);
+    delay(2500);            //2.5s between rings
   }
 }
 
